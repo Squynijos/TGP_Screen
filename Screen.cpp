afa4b966ca026dbfeeb2394b98df4643a89af452
Squynijos <81109473+Squynijos@users.noreply.github.com>
2023-02-23 12:24:33 -0500

    Update Screen.cpp

--- Screen.cpp
+++ Screen.cpp
@@ -2,6 +2,11 @@
 
 Screen::Screen(int8_t reset_pin) : Ecran(reset_pin) {}
 
+void Screen :: effacerUp(byte column)
+{
+  fillRect(column*8, 0, 8, 64, BLACK);
+}
+
 void Screen :: drawSideDimmer(float valeur,float min, float max, byte ligne)
 {
   valeur = (valeur-min) * 128/(max-min);
@@ -9,8 +14,6 @@ void Screen :: drawSideDimmer(float valeur,float min, float max, byte ligne)
   drawRect(0, ligne*8, 128, 8, WHITE);
   fillRect(0, ligne*8, valeur, 8, WHITE);
 }
-
-//void Screen :: drawTriDimmer (float valeur,float min, float max, bool)
 
 void Screen :: drawUpDimmer(float valeur,float min, float max, byte column)
 {
@@ -20,7 +23,4 @@ void Screen :: drawUpDimmer(float valeur,float min, float max, byte column)
   fillRect(column*8, 64-valeur, 8, valeur, WHITE);
 }
 
-void Screen :: effacerUp(byte column)
-{
-  fillRect(column*8, 0, 8, 64, BLACK);
-}
+//void Screen :: drawTriDimmer (float valeur,float min, float max, bool)
