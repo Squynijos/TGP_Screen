d252359767fc56ec341c843cf6a57aba8984041a
ragrande <74555049+ragrande@users.noreply.github.com>
2023-02-23 11:40:35 -0500

    Update Screen.cpp

--- Screen.cpp
+++ Screen.cpp
@@ -2,5 +2,12 @@
 
 Screen::Screen(int8_t reset_pin) : Ecran(reset_pin) {}
 
-
+void Screen :: drawRectDimer(float valeur,float min, float max, int ligne)
+{
+  ligne = ligne - 1; // permet d'ecrire ligne 1 au lieu de ligne 0 pour 1
+  valeur = valeur * 128/(max-min);
+  effacer(ligne);
+  drawRect(0, ligne*8, 128, 8, WHITE);
+  fillRect(0, ligne*8, valeur, 8, WHITE);
+}
 
