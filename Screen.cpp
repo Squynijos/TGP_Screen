a644cbaad926f0d397da081e11907c9da7dcafa4
Squynijos <81109473+Squynijos@users.noreply.github.com>
2023-02-23 11:42:58 -0500

    Correct Spelling

--- Screen.cpp
+++ Screen.cpp
@@ -2,7 +2,7 @@
 
 Screen::Screen(int8_t reset_pin) : Ecran(reset_pin) {}
 
-void Screen :: drawRectDimer(float valeur,float min, float max, int ligne)
+void Screen :: drawRectDimmer(float valeur,float min, float max, int ligne)
 {
   ligne = ligne - 1; // permet d'ecrire ligne 1 au lieu de ligne 0 pour 1
   valeur = valeur * 128/(max-min);
