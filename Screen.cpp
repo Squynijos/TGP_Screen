3cb82ee11af86ec50ced6c02310ad8c9c9ef2626
ragrande <74555049+ragrande@users.noreply.github.com>
2023-02-23 11:58:38 -0500

    Update Screen.cpp

--- Screen.cpp
+++ Screen.cpp
@@ -2,12 +2,25 @@
 
 Screen::Screen(int8_t reset_pin) : Ecran(reset_pin) {}
 
-void Screen :: drawRectDimmer(float valeur,float min, float max, int ligne)
+void Screen :: drawSideDimmer(float valeur,float min, float max, byte ligne)
 {
-  ligne = ligne - 1; // permet d'ecrire ligne 1 au lieu de ligne 0 pour 1
-  valeur = valeur * 128/(max-min);
+  valeur = (valeur-min) * 128/(max-min);
   effacer(ligne);
   drawRect(0, ligne*8, 128, 8, WHITE);
   fillRect(0, ligne*8, valeur, 8, WHITE);
 }
 
+//void Screen :: drawTriDimmer (float valeur,float min, float max, bool)
+
+void Screen :: drawUpDimmer(float valeur,float min, float max, byte column)
+{
+  valeur = (valeur-min) * 64/(max-min);
+  effacerUp(column);
+  drawRect(column*8, 0, 8, 64, WHITE);
+  fillRect(column*8, 0, 8, valeur, WHITE);
+}
+
+void Screen :: effacerUp(byte column)
+{
+    fillRect(colomn*8, 0, 8, 64, BLACK);
+}
